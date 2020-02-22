b756ee69eb721c939317a7e2191d99a188cd5a3f
Alex Falkowski <alexrfalkowski@gmail.com>
2020-02-22 07:03:23 +0000

    test: move setup to step

--- features/step_definitions/echo_steps.rb
+++ features/step_definitions/echo_steps.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+Given('we configure nonnative to startup') do
+  Nonnative.configure do |config|
+    config.strategy = :startup
+
+    config.definition do |d|
+      d.process = 'features/support/bin/start 12_321'
+      d.timeout = 0.5
+      d.port = 12_321
+      d.file = 'logs_12_321'
+    end
+
+    config.definition do |d|
+      d.process = 'features/support/bin/start 12_322'
+      d.timeout = 0.5
+      d.port = 12_322
+      d.file = 'logs_12_322'
+    end
+  end
+end
+
 When('we send {string} with the echo client') do |message|
   @responses = []
   @responses << Nonnative::EchoClient.new(12_321).request(message)
--- features/support/env.rb
+++ features/support/env.rb
@@ -7,21 +7,3 @@ SimpleCov.start do
 end
 
 require 'nonnative'
-
-Nonnative.configure do |config|
-  config.strategy = :startup
-
-  config.definition do |d|
-    d.process = 'features/support/bin/start 12_321'
-    d.timeout = 0.5
-    d.port = 12_321
-    d.file = 'logs_12_321'
-  end
-
-  config.definition do |d|
-    d.process = 'features/support/bin/start 12_322'
-    d.timeout = 0.5
-    d.port = 12_322
-    d.file = 'logs_12_322'
-  end
-end
