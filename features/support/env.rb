ea109fbb3fba2c8fc23df5b582a83d373c77fa6f
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-04-27 22:19:44 +0200

    test: integrate coveralls (#78)
    * test(deps): add coveralls (0.8.23)
    
    * test: add coveralls config
    
    * test: send coveralls data
    
    * docs: add coveralls badge

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'coveralls'
 
+formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
 SimpleCov.start do
   add_filter '/features/'
 end
